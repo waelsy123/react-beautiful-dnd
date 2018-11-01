2946b5b047d88ea2fc70b4151a40699c11b748b2
Alex Reardon <areardon@atlassian.com>
2018-11-01 16:25:25 +1100

    temp disabling table

--- .storybook/config.js
+++ .storybook/config.js
@@ -14,11 +14,11 @@ function loadStories() {
 
 configure(loadStories, module);
 
-const table = console.table || console.log;
+// const table = console.table || console.log;
 
-console.log('environment');
-table([
-  ['react-beautiful-dnd version', version],
-  ['react version', React.version],
-  ['process.env.NODE_ENV', process.env.NODE_ENV],
-]);
+// console.log('environment');
+// table([
+//   ['react-beautiful-dnd version', version],
+//   ['react version', React.version],
+//   ['process.env.NODE_ENV', process.env.NODE_ENV],
+// ]);
